ace896f6d5ae8b33cac5bd55f4bcca682699a517
FathiAmzar <143429862+FathiAmzar@users.noreply.github.com>
2026-02-06 21:42:41 +0800

    Update ALB listener configuration and health check

--- infra/alb.tf
+++ infra/alb.tf
@@ -16,6 +16,7 @@ resource "aws_lb_target_group" "main" {
   health_check {
     path = "/"
     matcher = "200-399"
+    timeout = 5
   }
 }
 
@@ -23,22 +24,6 @@ resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.main.arn
   port              = "80"
   protocol          = "HTTP"
-
-  default_action {
-    type = "redirect"
-    redirect {
-      port        = "443"
-      protocol    = "HTTPS"
-      status_code = "HTTP_301"
-    }
-  }
-}
-
-resource "aws_lb_listener" "https" {
-  load_balancer_arn = aws_lb.main.arn
-  port              = "443"
-  protocol          = "HTTPS"
-  certificate_arn   = var.acm_cert_arn
 
   default_action {
     type             = "forward"
